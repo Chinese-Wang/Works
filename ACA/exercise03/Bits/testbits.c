d45c9f1b789f70dac493b825524d7464678ec3a5
Laboratory <w13723068323@163.com>
2022-03-15 15:47:51 +0800

    final

--- ACA/exercise03/Bits/testbits.c
+++ ACA/exercise03/Bits/testbits.c
@@ -15,27 +15,27 @@ int main( ) {
     x.inFloats[0] = 4.8;
     x.inFloats[1] = 256.0;
 
-    printf("byte: ");
+    printf("byte: ");  //154       153     153     64      0       0       128     67
     for (int i = 0; i < nBytes; i++)
         printf("%d\t", x.byte[i]);
     
-    printf("\ninDouble: %lf", x.inDouble);
+    printf("\ninDouble: %lf", x.inDouble); //144115222757716800.000000
 
-    printf("\ninFloats:%f\t%f", x.inFloats[0], x.inFloats[1]);
+    printf("\ninFloats:%f\t%f", x.inFloats[0], x.inFloats[1]); //4.800000       256.000000
 
-    printf("\ninLong:%lld", x.inLong);
+    printf("\ninLong:%lld", x.inLong); //4863887598643943834
 
-    printf("\nunLong:%lld", x.unLong);
+    printf("\nunLong:%lld", x.unLong); //4863887598643943834
 
-    printf("\ninInts:%d\t%d", x.inInts[0], x.inInts[1]);
+    printf("\ninInts:%d\t%d", x.inInts[0], x.inInts[1]); //1083808154       1132462080
 
-    printf("\nunInts:%d\t%d", x.unInts[0], x.unInts[1]);
+    printf("\nunInts:%d\t%d", x.unInts[0], x.unInts[1]); //1083808154       1132462080
 
-    printf("\ninShorts:");
+    printf("\ninShorts:"); //-26214 16537   0       17280
     for (int i = 0; i < nShort; i++)
         printf("%d\t", x.inShorts[i]);
 
-    printf("\nunShorts:");
+    printf("\nunShorts:"); //39322  16537   0       17280
     for (int i = 0; i < nShort; i++)
         printf("%d\t", x.unShorts[i]);
     
